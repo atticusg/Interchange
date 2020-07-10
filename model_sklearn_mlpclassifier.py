a2bfa4c13a0d5ca98438ac1d497a02ed3197f2e0
Hanson Lu <hansonlu.hl@gmail.com>
2020-07-10 14:37:22 -0500

    start implementing mlpclassifier

--- model_sklearn_mlpclassifier.py
+++ model_sklearn_mlpclassifier.py
@@ -1,37 +1,34 @@
-from abc import ABC, abstractmethod
+from model import Model
 
-class Model(ABC):
-    def __init__(self):
+class Sklearn_MLPClassifier_Model(Model):
+    def __init__(self, mlp_classifier_model):
+        self.model = mlp_classifier_model
+
         pass
 
-    @abstractmethod
     def init_vector_map(self):
         """ Here the user defines how to map strings to acceses vectors in the model """
+
         pass
 
-    @abstractmethod
     def get_vector(self, name):
         """ Obtain the current values of an internal vector in the model """
         pass
 
-    @abstractmethod
     def set_vector(self, name, value):
         """ Set the values of an internal vector """
         pass
 
-    @abstractmethod
     def run(self, inputs):
         """ Run the model on a set of inputs, we expect this to update *all*
         internal vectors of the model according to the new input"""
         pass
 
-    @abstractmethod
     def fix_and_run(self, names, inputs):
         """ Run the model on a set of inputs, but hold a set of internal vectors
         constant. This set is given by the list of strings in `names`"""
         pass
 
-    @abstractmethod
     def get_causal_ordering(self, name):
         """ Get a data structure of what downstream vectors are affected by
         one internal vector designated by `name`"""
