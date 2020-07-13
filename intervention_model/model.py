801edd5237ab095e0f6ac8779b03be5c8975fc6c
Hanson Lu <hansonlu.hl@gmail.com>
2020-07-13 17:03:43 -0500

    added ComputationGraphNode class

--- intervention_model/model.py
+++ intervention_model/model.py
@@ -1,21 +1,21 @@
 from abc import ABC, abstractmethod
 
-class Model(ABC):
+class InterventionModel(ABC):
     def __init__(self):
         pass
 
     @abstractmethod
-    def init_vector_map(self):
+    def update_vector_cache(self):
         """ Here the user defines how to map strings to acceses vectors in the model """
         pass
 
     @abstractmethod
-    def get_vector(self, name):
+    def get_from_cache(self, name):
         """ Obtain the current values of an internal vector in the model """
         pass
 
     @abstractmethod
-    def set_vector(self, name, value):
+    def set_to_cache(self, name, value):
         """ Set the values of an internal vector """
         pass
 
@@ -28,11 +28,31 @@ class Model(ABC):
     @abstractmethod
     def fix_and_run(self, names, inputs):
         """ Run the model on a set of inputs, but hold a set of internal vectors
-        constant. This set is given by the list of strings in `names`"""
+        constant. This set is given by the list of strings in `names`.
+
+        Currently, the best way to implement this function for pytorch modules
+        that inherit from the `nn.Module` class is to re-write the forward()
+        function.
+        """
         pass
 
     @abstractmethod
     def get_causal_ordering(self, name):
         """ Get a data structure of what downstream vectors are affected by
         one internal vector designated by `name`"""
+        pass
+
+class ComputationGraphNode(ABC):
+    def __init__(self, name, inputs):
+        self.name = name
+        self.inputs = inputs
+        self.cache = None
+        pass
+
+    @abstractmethod
+    def forward():
+        pass
+
+    @abstractmethod
+    def forward_with_cache():
         pass
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="intervention_model",
+    version="0.0.1",
+    description="Tools for implementing and running neural network internvention experiments",
+    author="Atticus Geiger, Chris Potts, Hanson Lu",
+    author_email="",
+    packages=["intervention_model"]
+)
