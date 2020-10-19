845d161facd83b14cda12d9b439dd1a3f0092169
Hanson Lu <hansonlu.hl@gmail.com>
2020-10-19 15:37:55 -0700

    remove excess shell files

--- run_abstraction.sh
+++
@@ -1,7 +0,0 @@
-python run_abstraction.py \
-    --data_path "mqnli_data/mqnli.pt" \
-    --model_path "mqnli_models/lstm_best.pt" \
-    --db_path "experiment_data/runtime.db" \
-    --res_save_dir "experiment_data/" \
-    --abstraction "abstractions.txt" \
-    --num_inputs 5
