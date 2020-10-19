8724d60c3530d841423ddf5861c38a98b02a9161
Hanson Lu <hansonlu@stanford.edu>
2020-10-19 15:22:49 -0700

    remove some script files

--- nrun_abstraction.sh
+++
@@ -1,6 +0,0 @@
-nlprun \
-    -a hanson-intervention \
-    -q john \
-    -o experiment_data/1000_run.log \
-    -r 100G \
-    './run_abstraction.sh'
--- nrun_grid_search.sh
+++
@@ -1,7 +0,0 @@
-NOW=$(date +"%Y%m%d-%H%M%S")
-LAYERS=$1
-nlprun \
-    -a hanson-intervention \
-    -g 1 \
-    -o experiment_data/sep_gs_${NOW}.log \
-    "python run_grid_search.py $LAYERS"
