09cc91dd7fd5331088512154c993952d1013abac
Hanson Lu <hansonlu.hl@gmail.com>
2020-11-24 21:32:52 -0800

    small bug

--- scripts/train_query.sh
+++ scripts/train_query.sh
@@ -4,5 +4,4 @@ DIFFICULTY=$2
 shift
 DATE=$3
 shift
-python train.py query \
-    -d "mqnli_models/${MODEL}-${DIFFICULTY}-${DATE}.db" "$@"
+python train.py query -d "mqnli_models/${MODEL}-${DIFFICULTY}-${DATE}.db" "$@"
