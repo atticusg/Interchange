ea91e5456f78d9c2712b4b872b7de8c06573a725
Hanson Lu <hansonlu.hl@gmail.com>
2020-12-03 21:56:45 -0800

    lstm-hard graph analysis script

--- scripts/expt_graph.sh
+++ scripts/expt_graph.sh
@@ -7,7 +7,8 @@ shift
 python experiment.py analyze_graph \
     -d "experiment_data/${MODEL}/${MODEL}-${DIFFICULTY}-${DATE}.db" \
     -i "python expt_graph.py" \
+    -x \
+    -m "nlprun -q john -a hanson-intervention" \
     -r 1 \
     "$@"
 # -n num_expts -s -2
-# -x -m "nlprun -q john -a hanson-intervention"
