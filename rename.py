167fa62967287ca2718017743a6688bd3faf15ad
Hanson Lu <hansonlu.hl@gmail.com>
2021-01-13 15:00:28 -0800

    rename interchange experiments

--- rename.py
+++ rename.py
@@ -6,12 +6,27 @@ def rename_experiment(path):
     for row in rows:
         # rename data paths
         update_dict = {}
-        update_dict["data_path"] = row["data_path"].replace("mqnli_data/mqnli-", "data/mqnli/preprocessed")
-        update_dict["data_path"] = update_dict["data_path"].replace("default", "easy")
+        data_path = row["data_path"]
+        data_path = data_path.replace("mqnli_data/mqnli_bert.pt", "data/mqnli/preprocessed/bert-easy.pt")
+        data_path = data_path.replace("mqnli_data/mqnli-", "data/mqnli/preprocessed/")
+        data_path = data_path.replace("default", "easy")
+        update_dict["data_path"] = data_path
 
-        update_dict["model_path"] = row["model_path"].replace("mqnli_models/", "data/models/")
+        model_path = row["model_path"]
+        model_path = model_path.replace("bert_best.pt", 'bert-easy-best.pt')
+        model_path = model_path.replace("mqnli_models/", "data/models/")
+        update_dict["model_path"] = model_path
+
+        for col in ["log_path", "res_save_dir", "save_path", "graph_save_paths", "visualize_save_paths"]:
+            if col not in row: continue
+            p = row[col]
+            if p:
+                p = p.replace("experiment_data/", "data/interchange/")
+                p = p.replace("hard-nov3", "hard")
+                update_dict[col] = p
 
         db.update(path, "results", update_dict, id=row["id"])
+    print(f"Updated {len(rows)} rows in db {path}")
 
 def rename_training(path):
     rows = db.select(path, "results")
@@ -27,11 +42,11 @@ def rename_training(path):
             update_dict["tokenizer_vocab_path"] = row["tokenizer_vocab_path"].replace("mqnli_data/", "data/tokenization/")
 
         for col in ["model_save_path", "res_save_dir", "log_path"]:
-            new_val = row[col]
-            new_val = new_val.replace("bert-hard-nov24", "bert-hard")
-            new_val = new_val.replace("bert-nov7", "bert-easy")
-            new_val = new_val.replace("mqnli_models/", "data/training/")
-            update_dict[col] = new_val
+            p = row[col]
+            p = p.replace("bert-hard-nov24", "bert-hard")
+            p = p.replace("bert-nov7", "bert-easy")
+            p = p.replace("mqnli_models/", "data/training/")
+            update_dict[col] = p
 
         db.update(path, "results", update_dict, id=row["id"])
     print(f"Updated {len(rows)} rows in db {path}")
@@ -54,6 +69,8 @@ def main():
     for path in paths:
         if "training" in path:
             rename_training(path)
+        if "data/interchange" in path:
+            rename_experiment(path)
         if path.endswith(".pt"):
             rename_checkpoint(path)
 
