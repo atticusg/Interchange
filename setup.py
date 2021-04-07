38cbd676ebc3a5a16da7b925ee6360b7417eab1d
jsrozner <1113285+jsrozner@users.noreply.github.com>
2021-04-07 14:04:59 -0700

    tweak interchange setup to enable finding other packages for import

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name="intervention_experiment",
+setuptools.setup(
+    # name="intervention_experiment",
+    name="interchange",
     version="0.0.1",
     description="Tools for implementing and running neural network internvention experiments",
     author="",
     author_email="",
-    packages=["intervention"]
+    # packages=["intervention"]
+    packages=setuptools.find_packages()
 )
