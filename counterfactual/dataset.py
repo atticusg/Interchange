7c5446bfb501067b2e4e6750197b3a834d19e856
Hanson Lu <hansonlu.hl@gmail.com>
2021-05-12 16:10:43 -0700

    random cf dataset working

--- counterfactual/dataset.py
+++ counterfactual/dataset.py
@@ -1,4 +1,5 @@
 import math
+import os
 from typing import *
 from collections import defaultdict
 import pickle
@@ -11,7 +12,7 @@ from tqdm import tqdm
 import antra
 from antra.utils import serialize
 from antra.interchange.mapping import AbstractionMapping
-from antra.counterfactual import CounterfactualDataset
+import antra.counterfactual.dataset as antra_cfd
 
 
 
@@ -78,7 +79,7 @@ class IsolatedPairFinder:
         ivn_high_node_idx = high_node2idx[list(intervened_high_nodes)[0]]
 
         pairs = []
-        for base_idx in base_idxs:
+        for base_idx in tqdm(base_idxs):
             base_input_tuple = base_dataset[base_idx]
             base_input_value = base_input_tuple[-2]
             input_len = base_input_value.shape[0]
@@ -131,3 +132,101 @@ class IsolatedPairFinder:
 
             high_model.clear_caches(base_gi)
         return pairs
+
+
+class MQNLICounterfactualDataset(antra_cfd.ListCounterfactualDataset):
+    def __init__(
+            self,
+            base_dataset,
+            high_model,
+            pairs,
+            mapping,
+            num_random_bases=100000,
+            num_random_ivn_srcs=20
+    ):
+        self.high_model = high_model
+        self.num_random_bases = num_random_bases
+        self.num_random_ivn_srcs = num_random_ivn_srcs
+        intervened_nodes = {n for n in mapping.keys() if n not in {"input", "root"}}
+        # assume only one node to intervene on for now
+        self.intervened_high_node = list(intervened_nodes)[0]
+
+        if isinstance(pairs, list):
+            intervention_pairs = pairs
+        elif pairs == "random":
+            intervention_pairs = self.get_random_ivn_pairs(base_dataset)
+        elif isinstance(pairs, str):
+            with open(pairs, "wb") as f:
+                intervention_pairs = pickle.load(f)
+        else:
+            raise ValueError(f"Invalid value for `pairs` {pairs}")
+
+        super(MQNLICounterfactualDataset, self).__init__(
+            base_dataset=base_dataset,
+            intervention_pairs=intervention_pairs,
+            mapping = mapping,
+            batch_dim = 0
+        )
+
+    def get_random_ivn_pairs(self, base_dataset):
+        print("Getting random intervention pairs")
+        base_idxs = torch.randperm(len(base_dataset))[:self.num_random_bases]
+        pairs = []
+        for base_idx in tqdm(base_idxs):
+            ivn_src_idxs = torch.randperm(len(base_dataset))[:self.num_random_ivn_srcs]
+            pairs.extend((base_idx.item(), ivn_src_idx.item()) for ivn_src_idx in ivn_src_idxs)
+        return pairs
+
+    def construct_intervention(self, base, ivn_source):
+        base_input_tensor = base[-2].unsqueeze(0)
+        ivn_input_tensor = ivn_source[-2].unsqueeze(0)
+        base_gi = antra.GraphInput({"input": base_input_tensor}, cache_results=False)
+        ivn_src_gi = antra.GraphInput({"input": ivn_input_tensor}, cache_results=False)
+        ivn_val = self.high_model.compute_node(self.intervened_high_node, ivn_src_gi)
+
+        return antra.Intervention(
+            base_gi, {self.intervened_high_node: ivn_val}, cache_results=False
+        )
+
+class MQNLIRandomIterableCfDataset(antra_cfd.RandomCounterfactualDataset):
+    def __init__(
+            self,
+            base_dataset,
+            high_model,
+            mapping,
+            num_random_bases=50000,
+            num_random_ivn_srcs=20
+    ):
+        self.high_model = high_model
+
+        intervened_nodes = {n for n in mapping.keys() if n not in {"input", "root"}}
+        # assume only one node to intervene on for now
+        self.intervened_high_node = list(intervened_nodes)[0]
+
+        super(MQNLIRandomIterableCfDataset, self).__init__(
+            base_dataset=base_dataset,
+            mapping = mapping,
+            batch_dim = 0,
+            num_random_bases=num_random_bases,
+            num_random_ivn_srcs=num_random_ivn_srcs
+        )
+
+    def construct_high_intervention(self, base_ex, ivn_src_ex):
+        base_input_tensor = base_ex[-2]
+        ivn_input_tensor = ivn_src_ex[-2]
+        base_gi = antra.GraphInput({"input": base_input_tensor}, cache_results=False)
+        ivn_src_gi = antra.GraphInput({"input": ivn_input_tensor.unsqueeze(0)}, cache_results=False)
+        ivn_val = self.high_model.compute_node(self.intervened_high_node, ivn_src_gi).squeeze(0)
+        return antra.Intervention(
+            base_gi, {self.intervened_high_node: ivn_val}, cache_results=False
+        )
+
+    def construct_low_input(self, example):
+        gi_dict = {
+            "input_ids": example[0],
+            "token_type_ids": example[1],
+            "attention_mask": example[2]
+        }
+
+        return antra.GraphInput(gi_dict, cache_results=False)
+
--- counterfactual/test_dataset.py
+++ counterfactual/test_dataset.py
@@ -1,9 +1,35 @@
 import torch
 import numpy as np
 
+import torch.utils.data as torch_data
 from antra import LOC
-from counterfactual.dataset import IsolatedPairFinder
+from counterfactual.dataset import MQNLIRandomIterableCfDataset
 from compgraphs.mqnli_logic import Full_MQNLI_Logic_CompGraph
+
+# def test_dataset_constructor():
+#     seed = 39
+#     np.random.seed(seed)
+#     torch.random.manual_seed(seed)
+#
+#     dataset_path = "data/mqnli/preprocessed/bert-hard_abl.pt"
+#     print("loading dataset")
+#     data = torch.load(dataset_path)
+#     base_dataset = data.train
+#     mapping = {
+#         "vp": {"bert_layer_3": LOC[:,10,:]}
+#     }
+#     pairs_pickle_path = "data/counterfactual/test_pairs.pk"
+#     high_model = Full_MQNLI_Logic_CompGraph(data)
+#     IsolatedPairFinder(
+#         Full_MQNLI_Logic_CompGraph,
+#         {"data": data}, base_dataset, mapping,
+#         pairs_pickle_path=pairs_pickle_path,
+#         num_base_inputs=200,
+#         num_ivn_srcs=32,
+#         pair_finding_batch_size=24,
+#         num_workers=2,
+#     )
+
 
 def test_dataset_constructor():
     seed = 39
@@ -17,15 +43,26 @@ def test_dataset_constructor():
     mapping = {
         "vp": {"bert_layer_3": LOC[:,10,:]}
     }
-    pairs_pickle_path = "data/counterfactual/test_pairs.pk"
     high_model = Full_MQNLI_Logic_CompGraph(data)
-    IsolatedPairFinder(
-        Full_MQNLI_Logic_CompGraph,
-        {"data": data}, base_dataset, mapping,
-        pairs_pickle_path=pairs_pickle_path,
-        num_base_inputs=200,
-        num_ivn_srcs=32,
-        pair_finding_batch_size=24,
-        num_workers=2,
+    dataset = MQNLIRandomIterableCfDataset(
+        base_dataset, high_model, mapping
     )
-    # 536, base=200, num_ivn_srcs=32, pair_finding_bs = 24
+    # sampler = torch_data.RandomSampler(dataset)
+    dataloader = torch_data.DataLoader(
+        dataset, batch_size=4, collate_fn=dataset.collate_fn)
+
+    for i, batch in enumerate(dataloader):
+        # print(batch)
+        if i == 500: break
+        low_base = batch["low_base_input"]
+        low_ivn_src = batch["low_intervention_source"]
+        # base_input_ids = low_base.values['input_ids']
+        # print(f"{base_input_ids.shape=}")
+
+        for j, (base_idx, ivn_src_idx) in enumerate(zip(batch['base_idxs'], batch['ivn_src_idxs'])):
+            base_ex = base_dataset[base_idx]
+            ivn_src_ex = base_dataset[ivn_src_idx]
+
+            for k, arg in enumerate(["input_ids", "token_type_ids", "attention_mask"]):
+                assert torch.allclose(base_ex[k], low_base[arg][j])
+                assert torch.allclose(ivn_src_ex[k], low_ivn_src[arg][j])
