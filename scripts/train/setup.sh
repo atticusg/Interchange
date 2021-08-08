bd99cba8ef94f541e4b3c7ce68d914b45d1a763d
Hanson Lu <hansonlu.hl@gmail.com>
2021-08-08 11:27:25 -0700

    running version of raw_bert training

--- scripts/train/setup.sh
+++ scripts/train/setup.sh
@@ -2,4 +2,4 @@ MODEL=$1
 DIFFICULTY=$2
 python train_manager.py setup \
     -d data/training/$MODEL-$DIFFICULTY.db \
-    -i data/mqnli/preprocessed/bert-$DIFFICULTY.pt
+    -i data/mqnli/preprocessed/$MODEL-$DIFFICULTY.pt
